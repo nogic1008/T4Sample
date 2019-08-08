fbbfe0b22c1b463b167c080e7a3fb2b05325cb71
nogic <24802730+nogic1008@users.noreply.github.com>
2019-08-08 16:27:58 +0900

    Call T4.TransformText at Program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 namespace T4Sample
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var typeDef = new Dictionary<string, string>()
+            {
+                { "integer", "int" },
+                { "varchar", "string" },
+                { "date", "DateTime" },
+            };
+
+            var table = new TableInfo()
+            {
+                Name = "staff_master",
+                Description = "Stores employee information.",
+                Columns = new[]
+                {
+                    new ColumnInfo() { Name = "staff_id", Type = "integer", IsPrimary = true, NotNull = true },
+                    new ColumnInfo() { Name = "staff_name", Type = "varchar", NotNull = true },
+                    new ColumnInfo() { Name = "address", Type = "varchar", Description = "Home Address." },
+                    new ColumnInfo() { Name = "created_date", Type = "date" },
+                }
+            };
+
+            ITextTemplate template = new TableEntityTemplate(typeDef, "MyNameSpace", table);
+
+            Console.WriteLine(template.TransformText());
         }
     }
 }
