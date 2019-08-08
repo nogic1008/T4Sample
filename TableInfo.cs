6857716873f29200dbbf3dbe3cee54e7a31d81fc
nogic <24802730+nogic1008@users.noreply.github.com>
2019-08-08 15:17:32 +0900

    remove Nullable annotation

--- TableInfo.cs
+++ TableInfo.cs
@@ -8,6 +8,6 @@ namespace T4Sample
 
         public IEnumerable<ColumnInfo> Columns { get; set; }
 
-        public string? Description { get; set; }
+        public string Description { get; set; }
     }
 }
